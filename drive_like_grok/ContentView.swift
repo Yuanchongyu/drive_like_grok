4aad1705b62d2f08387e633d1e6f1f1b7a44f515
jxa98 <jxa98@sfu.ca>
2026-03-10 20:25:04 -0700

    Initial Commit

---
+++ drive_like_grok/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  drive_like_grok
+//
+//  Created by chongyuyuan on 2026-03-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ drive_like_grok/drive_like_grokApp.swift
@@ -0,0 +1,17 @@
+//
+//  drive_like_grokApp.swift
+//  drive_like_grok
+//
+//  Created by chongyuyuan on 2026-03-10.
+//
+
+import SwiftUI
+
+@main
+struct drive_like_grokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
